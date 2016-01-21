51841e0dcec32ed78ddee6c8cc72b9adce7f67e1
Kevin Murphy <kevin@retroneogames.com>
2016-01-21 20:53:26 +0000

    commit to revert
    failed at a cursor lock mode thing

---
+++ Assets/CursorLockModeWeb.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CursorLockModeWeb : Button{
+
+	public override void OnPointerDown (UnityEngine.EventSystems.PointerEventData eventData)
+	{
+		base.OnPointerDown (eventData);
+		Cursor.lockState = CursorLockMode.Confined;
+	}
+}
